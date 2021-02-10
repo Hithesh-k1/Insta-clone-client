3e8cf99c0d4b793d26afa8a89a65b7b91c3ad720
hithesh-k <hithesh.kumar@nineleaps.com>
2021-02-10 19:59:56 +0530

    [ Login ] feature added onChange

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,24 @@
 import React from "react";
+import useInput from "../hooks/useInput";
 
 const Login = () => {
+  const email = useInput("");
+  const password = useInput("");
   return (
     <form>
       <form>
-        <input type="email" placeholder="email" />
-        <input type="password" placeholder="password" />
+        <input
+          type="email"
+          placeholder="email"
+          value={email.value}
+          onChange={email.onChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password.value}
+          onChange={password.onChange}
+        />
         <input type="submit" value="Log In" className="login-btn" />
       </form>
 
