78c0e9f09450e6b45a2ff16c2bd79bd8dce02540
hithesh-k <hithesh.kumar@nineleaps.com>
2021-02-10 19:49:25 +0530

    [ Login ] feature added login structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div>
-      <h1>hello </h1>
+      <Login />
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <form>
+      <form>
+        <input type="email" placeholder="email" />
+        <input type="password" placeholder="password" />
+        <input type="submit" value="Log In" className="login-btn" />
+      </form>
+
+      <div>
+        <p>
+          Don't have an account? <span onClick={signup}>Sign up</span>
+        </p>
+      </div>
+    </form>
+  );
+};
+
+export default Login;
