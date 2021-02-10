086c7d200fdeef4fe3ecd5e6c4e2ef992fe2d091
hithesh-k <hithesh.kumar@nineleaps.com>
2021-02-10 20:10:11 +0530

    [ Login ] feature added handleLogin function

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,36 @@
 import React from "react";
+import { toast } from "react-toastify";
 import useInput from "../hooks/useInput";
 
 const Login = () => {
   const email = useInput("");
   const password = useInput("");
+   const handleLogin = async (e) => {
+    e.preventDefault();
 
+    if (!email.value || !password.value) {
+      return toast.error("Please fill in both the fields");
+    }
+
+    const body = { email: email.value, password: password.value };
+
+    try {
+      const { token } = await client("/auth/login", { body });
+      localStorage.setItem("token", token);
+    } catch (err) {
+      return toast.error(err.message);
+    }
+
+    const user = await client("/auth/me");
+    localStorage.setItem("user", JSON.stringify(user.data));
+    setUser(user.data);
+    toast.success("Login successful");
+
+    email.setValue("");
+    password.setValue("");
+  };
   return (
-    <form >
+    <form onSubmit={handleLogin}>
       <form>
         <input
           type="email"
